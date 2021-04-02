ec769dc0bfa81f770a3e26600932375effd48ce2
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2021-04-02 21:57:06 +0500

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,4 +1,4 @@
-//A node class which creates the nodes for the linked List along basic getter and setter functions
+//A node class which creates the nodes for the Spell Corrector
 
 #include<iostream>
 using namespace std;
