8fbea550f51df42f9c4bd6789a4e110abc9fcb33
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2021-04-14 19:04:00 +0500

    Spell Correcter header File

---
+++ Spell_Correcter.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"BST.h"
+#ifndef SPELL
+#define SPELL
+using namespace std;
+
+
+class Spell_Correcter
+{
+private:
+	BST tree;
+public:
+	void load_dict();
+	string spell_checker(string address);
+	bool spell_correcor(string address);
+	string preprocessing(string s);
+	void get_suggestion(Node* t, string word);
+
+};
+
+#endif
